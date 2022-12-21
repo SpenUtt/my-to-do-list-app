7691641fb7d2e6a5286e2d3bdf79bb60b1b05c22
SpenUtt <spencerutt8@gmail.com>
2022-12-21 15:02:45 +0100

    converted js to jquery

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+function newItem(){
+//1. adding a new to do list item
+
+    let list = $('#list');
+    let li = $('<li></li>');
+    let userInput = $('#input');
+    let inputValue = userInput.val();
+    li.append(inputValue);
+
+    if (!inputValue) {
+        alert('You must write something!');
+    } else {
+        list.append(li);
+        userInput.val('');
+    }
+
+//2. Crossing out an item from the list of items: 
+    function crossOut() {
+        li.addClass('strike');
+    }
+    li.on('dblclick', crossOut);
+
+//3.1. adding the delete button "X":
+    let crossOutButton = $('<crossOutButton>');
+    li.append(crossOutButton);
+
+//3.2. adding class delete (display: none) from the css: 
+    function deleteListItem() {
+        li.addClass('delete');
+    }
+    crossOutButton.on('click', deleteListItem);
+
+//4. reordering the items: 
+    list.sortable();
+}
